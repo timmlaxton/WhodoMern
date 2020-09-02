8292233cc6f3c5987d6b6ac8f56298e24ba2f03a
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-09-02 12:42:02 +0100

    CRUD routes added

---
+++ models/Todo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+    
+  },
+  attention: {
+    type: Boolean,
+    required: true
+    
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('todo', TodoSchema)
--- routes/todos.js
+++ routes/todos.js
@@ -1,33 +1,122 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth')
+const {check, validationResult} = require('express-validator/check');
 
-// @route   GET api/todos
-// @desc    Get all todos
-// @access  Private  
-router.get('/', (req, res) => {
-  res.send('Get all Todos')
+const User = require('../models/User');
+const Todo = require('../models/Todo');
+
+// @route   GET  api/todos
+// @desc    Get all  todo
+// @access  Public
+
+router.get('/', auth, async (req, res) => {
+ try {
+   const todos = await Todo.find({ user: req.user.id }).sort({ date: -1 })
+   res.json(todos)
+ } catch (err) {
+   console.error(err.message);
+   res.status(500).send('Server Error')
+ }
 });
 
 
-// @route   POST api/todos
-// @desc    Add new todos
-// @access  Private  
-router.post('/', (req, res) => {
-  res.send('Add Todos')
+
+// @route   POST  api/auth
+// @desc    Add new todo
+// @access  Private
+
+router.post('/', [
+  auth, 
+  [
+  check('name', 'Name is required')
+  .not()
+  .isEmpty()
+] 
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array() })
+   }
+
+   const {name, message, attention}  = req.body;
+
+   try {
+     const newTodo = new Todo({
+       name,
+       message,
+       attention,
+       user:  req.user.id
+     });
+
+     const todo = await newTodo.save();
+     res.json(todo);
+   } catch (err) {
+     console.error(err.message);
+     res.status(500).send('Server Error')
+   }
 });
 
-// @route   PUT api/todos
-// @desc    Update  todos
-// @access  Private  
-router.put('/:id', (req, res) => {
-  res.send('Update Todos')
+// @route   PUT  api/todos/:id
+// @desc    Update todos
+// @access  Private
+
+router.put('/:id', auth, async (req, res) => {
+  const {name, message, attention}  = req.body;
+
+  //Build a contact object
+  const todoFields = {};
+  if(name) todoFields.name = name;
+  if(message) todoFields.message = message;
+  if(attention) todoFields.attention = attention;
+  
+
+  try {let todo = await Todo.findById(req.params.id);
+
+    if(!todo) return res.status(404).json({msg: 'Todo not found'})
+
+    // make sure user owns contact
+    if(todo.user.toString() !== req.user.id){
+      return res.status(401).json({ msg: 'Not authorized'})
+    }
+
+    todo = await Todo.findByIdAndUpdate(req.params.id,
+      { $set: todoFields},
+      {new: true});
+
+      res.json(todo);
+  } catch (err) {
+    console.error(err.message);
+     res.status(500).send('Server Error')
+  }
 });
 
-// @route   DELETE api/todos
-// @desc    Delete  todos
-// @access  Private  
-router.delete('/:id', (req, res) => {
-  res.send('Delete Todos')
+// @route   PUT  api/todos/:id
+// @desc    Delete todos
+// @access  Private
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let todo = await Todo.findById(req.params.id);
+
+    if(!todo) return res.status(404).json({msg: 'Todo not found'})
+
+    // make sure user owns todo
+    if(todo.user.toString() !== req.user.id){
+      return res.status(401).json({ msg: 'Not authorized'})
+    }
+
+    await Todo.findByIdAndRemove(req.params.id)
+
+      res.json({  msg: 'Todo removed'});
+  } catch (err) {
+    console.error(err.message);
+     res.status(500).send('Server Error')
+  }
 });
 
-module.exports = router
+
+
+
+module.exports = router;
+
