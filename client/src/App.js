bdac6ad5ffcd2c9cf4aeb4448fcf87ddc7cf33df
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-09-17 12:45:30 +0100

    added dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, {Fragment, useEffect} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import SearchBar from './components/layout/SearchBar';
+import NavBar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
 import Todos from './components/todos/Todos';
-
-
-
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -14,12 +14,12 @@ const App = () => {
     // Init Materialize JS
     M.AutoInit();
   });
-    return <Fragment>
-      <SearchBar/>
-      <div className='container'>
-      <Todos />
-      </div>
+    return (
+      <Fragment>
+      <NavBar/>
+      <Landing/>
     </Fragment>
+    )
 };
 
 
---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Whodo</h1>
+          <p className="lead">
+            Make Notes - Track Progress - Get Things Done
+          </p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">Sign Up</a>
+            <a href="login.html" className="btn btn-light">Login</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Landing
