bb23457fe09e368e724adda216ec7a98e5d6b17d
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-09-21 11:09:23 +0100

    added alert to registration

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import NavBar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+import Alert from './components/layout/Alert'
 //import Todos from './components/todos/Todos';
 
 // import 'materialize-css/dist/css/materialize.min.css';
@@ -26,6 +27,7 @@ const App = () => (
       <NavBar/>
       <Route exact path='/' component={Landing} />
       <section className="container">
+        <Alert/>
         <Switch>
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, {Fragment, useState} from 'react'
-import{Link} from 'react-router-dom'
+import { connect } from 'react-redux'
+import{ Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types'
 
 
-const Register = () => {
+
+const Register = ({setAlert}) => {
 
   const [formData, setFormData] = useState({
     name: '',
@@ -18,7 +22,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log('success')
       }
@@ -66,4 +70,8 @@ const Register = () => {
   )
 }
 
-    export default Register
+Register.prototype = {
+  setAlert: PropTypes.func.isRequired
+}
+
+    export default connect(null, { setAlert })(Register);
